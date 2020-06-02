b40c3d1e45511fe465311b3a45bc6bc9261e468c
Elizabeth5 <escheidt5@gmail.com>
2020-06-02 09:16:04 -0400

    refactor

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
-let rows = document.querySelectorAll(".row")
-let searchSubmit = document.querySelector("#submit-search")
-let searchInputElement = document.querySelector("#search-input")
-let replaceInputElement = document.querySelector("#replace-input")
+let rows = document.querySelectorAll(".row");
+let searchSubmit = document.querySelector("#submit-search");
+let searchInputElement = document.querySelector("#search-input");
+let replaceInputElement = document.querySelector("#replace-input");
 
-searchSubmit.addEventListener("click", function (){
-    let searchInputValue = searchInputElement.value
-    let replaceInputValue = replaceInputElement.value
-    for(let i = 0; i < rows.length; i++){
-        let currentRow = rows[i]
-        let cells = currentRow.querySelectorAll(".cell")
-        // for(let j = 0; j < cells.length; j++){
-        //     let currentCell = cells[j]
-        //     if(currentCell.innerHTML === searchInputValue){
-        //         currentCell.innerHTML = replaceInputValue
-        //         return
-        //     }
-        // }
+searchSubmit.addEventListener("click", function () {
+  let searchInputValue = searchInputElement.value;
+  let replaceInputValue = replaceInputElement.value;
+  let cells = document.querySelectorAll(".cell");
+  for (let i = 0; i < cells.length; i++) {
+    let currentCell = cells[i];
+    if (currentCell.innerHTML === searchInputValue) {
+      currentCell.innerHTML = replaceInputValue;
     }
-    
-})
-
-
-
+  }
+  //   for (let i = 0; i < rows.length; i++) {
+  //     let currentRow = rows[i];
+  //     let cells = currentRow.querySelectorAll(".cell");
+  // for(let j = 0; j < cells.length; j++){
+  //     let currentCell = cells[j]
+  //     if(currentCell.innerHTML === searchInputValue){
+  //         currentCell.innerHTML = replaceInputValue
+  //         return
+  //     }
+  // }
+  //}
+});
